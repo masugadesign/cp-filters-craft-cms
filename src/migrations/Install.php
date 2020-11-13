adf279fc7062c40256c01fa4f282ac8da212849e
Catherine Kleimeier <catherine@masugadesign.com>
2020-11-13 16:44:45 -0500

    Create install migration

---
+++ src/migrations/Install.php
@@ -0,0 +1,30 @@
+<?php
+namespace Masuga\CpFilters\migrations;
+
+use craft\db\Migration;
+
+class Install extends Migration
+{
+	public function safeUp()
+	{
+		if (!$this->db->tableExists('{{%cpfilters_savedfilters}}')) {
+			$this->createTable('{{%cpfilters_savedfilters}}', [
+				'id' => $this->primaryKey(),
+				'elementId' => $this->integer(),
+				'userId' => $this->integer(),
+				'title' => $this->string(255),
+				'filterUrl' => $this->string(255),
+				'dateCreated' => $this->dateTime()->notNull(),
+				'dateUpdated' => $this->dateTime()->notNull(),
+				'uid' => $this->uid()
+			]);
+		}
+	}
+
+	public function safeDown()
+	{
+		if ( $this->db->tableExists('{{%cpfilters_savedfilters}}') ) {
+			$this->dropTable('{{%cpfilters_savedfilters}}');
+		}
+	}
+}
