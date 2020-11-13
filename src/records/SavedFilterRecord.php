d1fe7322a29b519a7e7f028e1af0dc225c62be2b
Catherine Kleimeier <catherine@masugadesign.com>
2020-11-13 16:42:56 -0500

    Create SavedFilterRecord

---
+++ src/records/SavedFilterRecord.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Masuga\CpFilters\records;
+
+use craft\db\ActiveRecord;
+use craft\base\Element;
+use craft\elements\User;
+use yii\db\ActiveQueryInterface;
+
+class SavedFilterRecord extends ActiveRecord
+{
+	/**
+	 * @inheritdoc
+	 */
+	public static function tableName(): string
+	{
+		return '{{%cpfilters_savedfilters}}';
+	}
+
+	/**
+	 * Returns the saved filter record's element.
+	 * @return ActiveQueryInterface The relational query object.
+	 */
+	public function getElement(): ActiveQueryInterface
+	{
+		return $this->hasOne(Element::class, ['id' => 'id']);
+	}
+
+	/**
+	 * Returns the saved filter record's related User.
+	 * @return ActiveQueryInterface The relational query object.
+	 */
+	public function getUser(): ActiveQueryInterface
+	{
+		return $this->hasOne(User::class, ['id' => 'userId']);
+	}
+
+}
