13c467d488c626a7a8aff99c089b9cd252f0dc9c
Catherine Kleimeier <catherine@masugadesign.com>
2020-11-13 16:43:23 -0500

    Create SavedFilters service
    This service creates and gets
    savedFilter elements

---
+++ src/services/SavedFilters.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Masuga\CpFilters\services;
+
+use Craft;
+use Exception;
+use craft\helpers\ArrayHelper;
+use craft\helpers\FileHelper;
+use Masuga\CpFilters\base\Service;
+use Masuga\CpFilters\elements\SavedFilter;
+use Masuga\CpFilters\elements\db\SavedFilterQuery;
+use Masuga\CpFilters\records\SavedFilterRecord;
+
+class SavedFilters extends Service
+{
+	/**
+	 * This method creates/updates a CPFilters SavedFilter element based on whether or
+	 * not an existing filter ID is supplied.
+	 * @param array $input
+	 * @param int $id
+	 * @return SavedFilter|null
+	 */
+	public function saveFilter($input=[], $id)
+	{
+		$savedFilter = null;
+		if ( $id ) {
+			$savedFilter = SavedFilter::find()->id($id)->one();
+		}
+		if ( ! $savedFilter ) {
+			$savedFilter = new SavedFilter;
+		}
+		$savedFilter->siteId = Craft::$app->getSites()->currentSite->id;
+		$savedFilter->title = $input['title'];
+		$savedFilter->criteria = $input['criteria'] ?? $savedFilter->criteria;
+		$savedFilter->orderBy = $input['orderBy'] ?? $savedFilter->orderBy;
+		$savedFilter->sort = $input['sort'] ?? $savedFilter->sort;
+		// On a successful save, return the savedFilter element itself.
+		return Craft::$app->getElements()->saveElement($savedFilter) ? $savedFilter : null;
+	}
+
+	/**
+	 * This method returns an array of saved filters based on provided criteria.
+	 * @param array $criteria
+	 * @return SavedFilterQuery
+	 */
+	public function getSavedFilters($criteria=array()): SavedFilterQuery
+	{
+		$query = SavedFilter::find();
+		if ($criteria) {
+			Craft::configure($query, $criteria);
+		}
+		return $query;
+	}
+
+
+}
