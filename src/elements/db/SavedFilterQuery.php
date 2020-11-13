af8b9dccc995f544518e97727f8793947bcd1463
Catherine Kleimeier <catherine@masugadesign.com>
2020-11-13 16:42:46 -0500

    Create SavedFilterQuery

---
+++ src/elements/db/SavedFilterQuery.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Masuga\CpFilters\elements\db;
+
+use craft\db\Query;
+use craft\elements\db\ElementQuery;
+use craft\helpers\Db;
+use Masuga\CpFilters\CpFilters;
+use Masuga\CpFilters\elements\SavedFilter;
+
+/**
+ */
+class SavedFilterQuery extends ElementQuery
+{
+	/**
+	 * Public properties
+	 */
+	public $elementId = null;
+	public $userId = null;
+	public $title = null;
+	public $filterUrl = null;
+
+	/**
+	 * The instance of the CP Filters plugin.
+	 * @var CPFilters
+	 */
+	private $plugin = null;
+
+	/**
+	* @inheritdoc
+	*/
+	protected $defaultOrderBy = ['cpfilters_savedfilters.dateCreated' => SORT_DESC];
+
+	/**
+	 * Override the established __set() method so we can add properties on-the-fly
+	 * from the init() method.
+	 * @param string $name
+	 * @param mixed $value
+	 */
+	public function __set($name, $value)
+	{
+		$this->{$name} = $value;
+	}
+
+	/**
+	 * Override the established __get() method so we can get properties on-the-fly
+	 * from the init() method.
+	 */
+	public function __call($name, $args)
+	{
+		if ( ! method_exists($this, $name) && property_exists($this, $name) && count($args) === 1 ) {
+			$this->$name = $args[0];
+			return $this;
+		}
+	}
+
+	public function init()
+	{
+		parent::init();
+		$this->plugin = CpFilters::getInstance();
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function with($value)
+	{
+		$this->with = $value;
+		return $this;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function beforePrepare(): bool
+	{
+		$this->joinElementTable('cpfilters_savedfilters');
+
+		$selectsArray = [
+			'cpfilters_savedfilters.userId',
+			'cpfilters_savedfilters.elementId',
+			'cpfilters_savedfilters.title',
+			'cpfilters_savedfilters.filterUrl'
+		];
+		$this->query->select($selectsArray);
+
+		if ($this->userId) {
+			$this->subQuery->andWhere(Db::parseParam('cpfilters_savedfilters.userId', $this->userId));
+		}
+		if ($this->elementId) {
+			$this->subQuery->andWhere(Db::parseParam('cpfilters_savedfilters.elementId', $this->elementId));
+		}
+		if ($this->title) {
+			$this->subQuery->andWhere(Db::parseParam('cpfilters_savedfilters.title', $this->userId));
+		}
+		if ($this->filterUrl) {
+			$this->subQuery->andWhere(Db::parseParam('cpfilters_savedfilters.filterUrl', $this->elementId));
+		}
+		return parent::beforePrepare();
+	}
+}
