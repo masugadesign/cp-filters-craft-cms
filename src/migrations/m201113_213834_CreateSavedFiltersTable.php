5bc60c1af1e20d9a0b60df1879785be4af58fd1f
Catherine Kleimeier <catherine@masugadesign.com>
2020-11-13 16:44:55 -0500

    Create CreateSavedFiltersTable migration

---
+++ src/migrations/m201113_213834_CreateSavedFiltersTable.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Masuga\CpFilters\migrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m201113_213834_CreateSavedFiltersTable migration.
+ */
+class m201113_213834_CreateSavedFiltersTable extends Migration
+{
+	public function safeUp()
+	{
+		if (!$this->db->tableExists('{{%cpfilters_savedfilters}}')) {
+			$this->createTable('{{%cpfilters_savedfilters}}', [
+				'id' => $this->primaryKey(),
+				'elementId' => $this->integer(),
+				'userId' => $this->integer(),
+				'title' => $this->string(255),
+				'filterUrl' => $this->string(255),
+				'dateCreated' => $this->dateTime()->notNull(),
+				'dateUpdated' => $this->dateTime()->notNull(),
+				'uid' => $this->uid()
+			]);
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function safeDown()
+	{
+		echo "m201113_213834_CreateSavedFiltersTable cannot be reverted.\n";
+		return false;
+	}
+}
