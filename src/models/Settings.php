ede246368c63643af2a3cdeae8bf310fe38172cc
me@benjaminkohl.com <me@benjaminkohl.com>
2021-02-01 15:27:18 -0500

    Fixed default setting values in the Settings model.

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -11,25 +11,25 @@ class Settings extends Model
 	 * An array of entry type IDs that should appear as filterable options.
 	 * @var mixed
 	 */
-	public $filterableEntryTypeIds = '*';
+	public $filterableEntryTypeIds = null;
 
 	/**
 	 * An array of Asset volume IDs that should appear as filterable options.
 	 * @var mixed
 	 */
-	public $filterableAssetVolumeIds = '*';
+	public $filterableAssetVolumeIds = null;
 
 	/**
 	 * An array of Category group IDs that should appear as filterable options.
 	 * @var mixed
 	 */
-	public $filterableCategoryGroupIds = '*';
+	public $filterableCategoryGroupIds = null;
 
 	/**
 	 * An array of Tag group IDs that should appear as filterable options.
 	 * @var mixed
 	 */
-	public $filterableTagGroupIds = '*';
+	public $filterableTagGroupIds = null;
 
 	/**
 	 * An array of additional field types that may be used as filter criteria.
